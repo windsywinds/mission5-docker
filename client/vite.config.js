3fdb60956210fd2847b415781e87ffa90123a103
windsywinds <windsor.sam@gmail.com>
2023-12-10 16:05:40 +1300

    added comments

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  //update your Vite config to allow access and prevent issues in the container
   server: {
     host: true,
     strictport: true,
